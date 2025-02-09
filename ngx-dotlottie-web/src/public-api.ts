98c5d9fd74071a2ba230ec0ca9de2dcae9b902ac
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-02-09 20:04:52 +0100

    feat(ngx-dotlottie-web): Add public API surface for ngx-dotlottie-web module

---
+++ ngx-dotlottie-web/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-dotlottie-web
+ */
+
+export * from './lib/components/dotlottie-web/dotlottie-web.component';
+export * from './lib/components/dotlottie-webworker/dotlottie-webworker.component';
+export * from './lib/providers/dotlottie-web.provider';
