6465c37333d2a3b8a826fac369e64f66db3cef54
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-02-09 20:24:02 +0100

    feat(ngx-dotlottie-demo): Add DotlottieWebControls component with HTML, CSS, and basic functionality

---
+++ ngx-dotlottie-demo/src/app/components/dotlottie-web-controls/dotlottie-web-controls.component.cy.ts
@@ -0,0 +1,7 @@
+import { DotlottieWebControlsComponent } from './dotlottie-web-controls.component';
+
+describe('DotlottieWebControlsComponent', () => {
+  it('should mount', () => {
+    cy.mount(DotlottieWebControlsComponent);
+  });
+});
---
+++ ngx-dotlottie-demo/src/app/components/dotlottie-web-controls/dotlottie-web-controls.component.ts
@@ -0,0 +1,55 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  input,
+  model,
+  output,
+  ViewEncapsulation,
+} from '@angular/core';
+import {
+  MatButtonToggle,
+  MatButtonToggleChange,
+  MatButtonToggleGroup,
+} from '@angular/material/button-toggle';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+import { DotLottieWebComponentInputType } from '../../../../../ngx-dotlottie-web/src/lib/@types/dotlottie-web';
+
+@Component({
+  selector: 'dotlottie-web-controls',
+  templateUrl: './dotlottie-web-controls.component.html',
+  styleUrl: './dotlottie-web-controls.component.css',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [MatButtonToggleGroup, MatButtonToggle, MatIcon, MatTooltip],
+  standalone: true,
+})
+export class DotlottieWebControlsComponent {
+  name = input.required<string>();
+
+  change = output<{
+    name: keyof DotLottieWebComponentInputType;
+    active: boolean;
+  }>();
+
+  model = model<Partial<DotLottieWebComponentInputType>>({});
+
+  controls: Array<{
+    name: keyof DotLottieWebComponentInputType;
+    icon: string;
+  }> = [
+    { name: 'play', icon: 'play_arrow' },
+    { name: 'stop', icon: 'stop_circle' },
+    { name: 'freeze', icon: 'ac_unit' },
+    { name: 'loop', icon: 'repeat' },
+    { name: 'autoResize', icon: 'aspect_ratio' },
+    { name: 'freezeOnOffscreen', icon: 'preview' },
+    { name: 'useFrameInterpolation', icon: '60fps' },
+  ];
+
+  onChange(change: MatButtonToggleChange) {
+    this.change.emit(change.value);
+  }
+}
