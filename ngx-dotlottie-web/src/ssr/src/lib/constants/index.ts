487edf4426f2517d3973da75a73b3460e61a703d
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-02-11 03:27:00 +0100

    chore(types): remove .d.ts from library use ts for declaration relying on compilation to produce .d.ts

--- ngx-dotlottie-web/src/ssr/src/lib/constants/index.ts
+++ ngx-dotlottie-web/src/ssr/src/lib/constants/index.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from '@angular/core';
-import { DotLottieWebSSROptions } from '../@types/dotlottie-web';
+import { DotLottieWebSSROptions } from '../@types/dotlottie-ssr';
 
 export const DOT_LOTTIE_WEB_SSR_OPTIONS =
   new InjectionToken<DotLottieWebSSROptions>('LottieServerOptions');
