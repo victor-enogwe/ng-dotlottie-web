65360ef2fd84ccffc7b0ff47e02e6d77fa4247e2
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-02-13 03:38:39 +0100

    test(jest): validate worker component workerId input

--- ngx-dotlottie-web/src/webworker/src/lib/components/dotlottie-webworker/dotlottie-webworker.component.spec.ts
+++ ngx-dotlottie-web/src/webworker/src/lib/components/dotlottie-webworker/dotlottie-webworker.component.spec.ts
@@ -1,7 +1,6 @@
 import type { ComponentFixture } from '@angular/core/testing';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
-import type { DotLottie, DotLottieWorker } from '@lottiefiles/dotlottie-web';
 import { resolve } from 'node:path';
 import type { DotLottieWebworkerComponentInputType } from '../../../../../web/src/lib/@types/dotlottie-web';
 import { DotLottieWebWorkerComponent } from './dotlottie-webworker.component';
@@ -33,69 +32,10 @@ describe.skip('DotLottieWebworkerComponent', () => {
     }).compileComponents();
   });
 
-  it('should create component with inputs', () => {
+  it('should create component with workerId input', () => {
     const { componentInstance } = createComponent({ src, workerId });
 
-    const hostClass = expect.objectContaining({
-      'd-block': true,
-      relative: true,
-    });
-
-    const canvasClass = expect.objectContaining({
-      'h-full': true,
-      'w-full': true,
-    });
-
-    expect(componentInstance).toBeTruthy();
-    expect(componentInstance.autoplay()).toBeTruthy();
-    expect(componentInstance.backgroundColor()).toEqual('#FFFFFF');
-    expect(componentInstance.src()).toBe(src);
-    expect(componentInstance.layout()).toBeUndefined();
-    expect(componentInstance.loop()).toBeTruthy();
-    expect(componentInstance.marker()).toBeUndefined();
-    expect(componentInstance.mode()).toBeUndefined();
-    expect(componentInstance.devicePixelRatio()).toEqual(undefined);
-    expect(componentInstance.autoResize()).toEqual(true);
-    expect(componentInstance.freezeOnOffscreen()).toEqual(true);
-    expect(componentInstance.segment()).toBeUndefined();
-    expect(componentInstance.speed()).toBeUndefined();
-    expect(componentInstance.themeId()).toBeUndefined();
-    expect(componentInstance.useFrameInterpolation()).toBeUndefined();
-    expect(componentInstance.canvasClass()).toEqual(canvasClass);
-    expect(componentInstance.hostClass()).toEqual(hostClass);
-    expect(componentInstance.freeze()).toBeFalsy();
-    expect(componentInstance.play()).toBeTruthy();
-    expect(componentInstance.stop()).toBeFalsy();
-    expect(componentInstance.workerId()).toBe(workerId);
-  });
-
-  it('should require the "src" input', () => {
-    const { componentInstance } = createComponent({ workerId });
-
     expect(componentInstance).toBeTruthy();
     expect(componentInstance.workerId()).toBe(workerId);
-    expect(componentInstance.src).toThrow();
-  });
-
-  it.skip('should should render a lottie file', async () => {
-    const src = resolve(fixturesDir, 'lottie.json');
-
-    const fixture = createComponent({ src, workerId });
-    const { componentInstance, componentRef, debugElement } = fixture;
-
-    // const lottie = await new Promise<DotLottie | DotLottieWorker | null>(
-    //   (resolve) => {
-    //     const subscription = componentInstance.lottieLoad.subscribe(
-    //       (lottie) => {
-    //         resolve(lottie);
-    //         subscription.unsubscribe();
-    //       },
-    //     );
-    //   },
-    // );
-
-    // expect(componentInstance).toBeTruthy();
-    // expect(componentInstance.workerId()).toBe(workerId);
-    // expect(lottie).toBeDefined();
   });
 });
