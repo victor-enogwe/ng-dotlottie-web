069df31416c2663567bb8715266c8514e824eff7
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-02-14 07:05:08 +0100

    feat: extend src type in DotLottieWebComponentInputType to include Config['data']

--- ngx-dotlottie-web/src/web/src/lib/@types/dotlottie-web.ts
+++ ngx-dotlottie-web/src/web/src/lib/@types/dotlottie-web.ts
@@ -16,7 +16,7 @@ export type AnimationFilename = string;
 export interface DotLottieWebComponentInputType {
   autoplay: Config['autoplay'];
   backgroundColor: Config['backgroundColor'];
-  src: Config['src'];
+  src: Config['src'] | Config['data'];
   layout: Config['layout'];
   loop: Config['loop'];
   marker: Config['marker'];
@@ -27,9 +27,9 @@ export interface DotLottieWebComponentInputType {
   segment: Config['segment'];
   speed: Config['speed'];
   themeId: Config['themeId'];
+  animationId: string | undefined;
   useFrameInterpolation: Config['useFrameInterpolation'];
   canvasClass: NgClass['ngClass'];
-  hostClass: NgClass['ngClass'];
   freeze: boolean;
   play: boolean;
   stop: boolean;
@@ -48,7 +48,7 @@ export interface DotLottieWebComponentInput {
     Config['backgroundColor']
   >;
 
-  src: InputSignal<Config['src']>;
+  src: InputSignal<Config['src'] | Config['data']>;
 
   layout: InputSignalWithTransform<Config['layout'], Config['layout']>;
 
@@ -79,14 +79,14 @@ export interface DotLottieWebComponentInput {
 
   themeId: InputSignalWithTransform<Config['themeId'], Config['themeId']>;
 
+  animationId: InputSignalWithTransform<Config['themeId'], Config['themeId']>;
+
   useFrameInterpolation: InputSignalWithTransform<
     Config['useFrameInterpolation'],
     Config['useFrameInterpolation']
   >;
 
   canvasClass: InputSignal<NgClass['ngClass']>;
-
-  hostClass: InputSignal<NgClass['ngClass']>;
 
   freeze: InputSignalWithTransform<boolean, boolean>;
 
