54fb851effa371f731302cf8e2b72835225bc6df
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-02-14 07:04:02 +0100

    test: enable AppComponent tests and improve teardown handling

--- ngx-dotlottie-demo/src/app/components/app/app.component.spec.ts
+++ ngx-dotlottie-demo/src/app/components/app/app.component.spec.ts
@@ -1,32 +1,46 @@
 import { TestBed } from '@angular/core/testing';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 import { AppComponent } from './app.component';
 
-describe.skip('AppComponent', () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      teardown: { destroyAfterEach: true },
       imports: [AppComponent],
+      providers: [provideNoopAnimations()],
     }).compileComponents();
   });
 
+  afterEach(() => TestBed.resetTestingModule());
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.detectChanges(true);
+
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it("should have the 'ngx-dotlottie-demo' title", () => {
     const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.detectChanges(true);
+
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ngx-dotlottie-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
     fixture.detectChanges();
+
     const compiled = fixture.nativeElement as HTMLElement;
+
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, ngx-dotlottie-demo',
+      'ngx-dotlottie-demo',
     );
   });
 });
