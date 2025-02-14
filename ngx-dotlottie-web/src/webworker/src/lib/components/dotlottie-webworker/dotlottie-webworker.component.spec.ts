b6ba95b67d94ccf324b23bba95b0b21786d95ece
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-02-14 07:06:34 +0100

    test(worker): add workerId input test

--- ngx-dotlottie-web/src/webworker/src/lib/components/dotlottie-webworker/dotlottie-webworker.component.spec.ts
+++ ngx-dotlottie-web/src/webworker/src/lib/components/dotlottie-webworker/dotlottie-webworker.component.spec.ts
@@ -1,30 +1,23 @@
-import type { ComponentFixture } from '@angular/core/testing';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
-import { resolve } from 'node:path';
-import type { DotLottieWebworkerComponentInputType } from '../../../../../web/src/lib/@types/dotlottie-web';
+import { setWasmURL } from '../../../../../ssr/src/lib/utils/set-wasm/set-wasm';
 import { DotLottieWebWorkerComponent } from './dotlottie-webworker.component';
 
-describe.skip('DotLottieWebworkerComponent', () => {
-  const rootDir = resolve(__dirname, '../../../../../');
-  const fixturesDir = resolve(rootDir, 'ngx-dotlottie-demo/cypress/fixtures');
-  const src = resolve(fixturesDir, 'lottie.json');
+describe('DotLottieWebworkerComponent', () => {
+  beforeAll(async () => setWasmURL());
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      teardown: { destroyAfterEach: true },
+      imports: [DotLottieWebWorkerComponent],
+    }).compileComponents();
+  });
+
+  afterEach(() => TestBed.resetTestingModule());
+
+  const src =
+    'https://lottie.host/0cbdb3ef-2fa5-4d1d-9e4e-f66c879e010d/D0bRr9d93F.lottie';
   const workerId = 'test';
-
-  function createComponent(
-    options: Partial<DotLottieWebworkerComponentInputType> = {},
-  ): ComponentFixture<DotLottieWebWorkerComponent> {
-    const fixture = TestBed.createComponent(DotLottieWebWorkerComponent);
-    const component = fixture.componentRef;
-
-    Object.entries(options).forEach(([key, value]) =>
-      component.setInput(key, value),
-    );
-
-    fixture.detectChanges();
-
-    return fixture;
-  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -33,7 +26,13 @@ describe.skip('DotLottieWebworkerComponent', () => {
   });
 
   it('should create component with workerId input', () => {
-    const { componentInstance } = createComponent({ src, workerId });
+    const fixture = TestBed.createComponent(DotLottieWebWorkerComponent);
+    const { componentRef, componentInstance } = fixture;
+
+    componentRef.setInput('src', src);
+    componentRef.setInput('workerId', workerId);
+
+    fixture.detectChanges();
 
     expect(componentInstance).toBeTruthy();
     expect(componentInstance.workerId()).toBe(workerId);
