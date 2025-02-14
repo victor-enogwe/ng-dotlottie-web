8a5d106a1f1380c4f1eb2320710545511cec43e3
victor-enogwe <23452630+victor-enogwe@users.noreply.github.com>
2025-02-14 07:04:12 +0100

    test: enable DotlottieWebCanvasComponent tests and improve teardown handling

--- ngx-dotlottie-demo/src/app/components/dotlottie-web-canvas/dotlottie-web-canvas.component.spec.ts
+++ ngx-dotlottie-demo/src/app/components/dotlottie-web-canvas/dotlottie-web-canvas.component.spec.ts
@@ -1,32 +1,22 @@
-import { expect } from '@jest/globals';
 import { TestBed } from '@angular/core/testing';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { expect } from '@jest/globals';
 import { DotlottieWebCanvasComponent } from './dotlottie-web-canvas.component';
 
-describe.skip('DotlottieWebCanvasComponent', () => {
+describe('DotlottieWebCanvasComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      teardown: { destroyAfterEach: true },
       imports: [DotlottieWebCanvasComponent],
+      providers: [provideNoopAnimations()],
     }).compileComponents();
   });
+
+  afterEach(() => TestBed.resetTestingModule());
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(DotlottieWebCanvasComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it("should have the 'ngx-dotlottie-demo' title", () => {
-    const fixture = TestBed.createComponent(DotlottieWebCanvasComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngx-dotlottie-demo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(DotlottieWebCanvasComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, ngx-dotlottie-demo',
-    );
   });
 });
